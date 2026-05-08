ab35771c7a652e8debade07538b93e508bed225a
vmnavarro94 <vm.navarro94@gmail.com>
2026-05-07 18:40:14 -0600

    Skip bluetooth enable if service not present

--- install/config/hardware/bluetooth.sh
+++ install/config/hardware/bluetooth.sh
@@ -1,2 +1,2 @@
-# Turn on bluetooth by default
-chrootable_systemctl_enable bluetooth.service
+# Turn on bluetooth by default (skip if no bluetooth hardware)
+systemctl list-unit-files bluetooth.service &>/dev/null && chrootable_systemctl_enable bluetooth.service || true
