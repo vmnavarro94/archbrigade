1bc8be17183ec212890d43b876c9cf4c2fb186e6
vmnavarro94 <vm.navarro94@gmail.com>
2026-05-07 18:00:33 -0600

    Keep sudo alive during AUR builds to prevent password timeout

--- install/packaging/aur.sh
+++ install/packaging/aur.sh
@@ -6,4 +6,10 @@ if pacman -Q rust &>/dev/null; then
   YAY_FLAGS+=(--assume-installed rustup)
 fi
 
+# Keep sudo alive during long AUR builds (some packages take minutes to compile)
+sudo -v
+(while true; do sudo -v; sleep 60; done) &
+SUDO_KEEPALIVE_PID=$!
+trap "kill $SUDO_KEEPALIVE_PID 2>/dev/null" EXIT
+
 yay -S "${YAY_FLAGS[@]}" "${packages[@]}"
